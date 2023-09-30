a15eae5ed9ebac81e1cec153ff82531659294917
mina ghoreshi <goreishi45@gmail.com>
2023-10-01 00:48:59 +0330

    add next functionality for first screen

--- js_modules/main.js
+++ js_modules/main.js
@@ -1,2 +1,6 @@
 import { fadeFirstPage } from "./fadeFirstPage.js";
+import { secondPage } from "./dom.js";
+import { nextPage } from "./nextPage.js";
 setTimeout(fadeFirstPage, 1000);
+
+secondPage.addEventListener("click", nextPage);
---
+++ js_modules/util.js
@@ -0,0 +1,14 @@
+export const screenData = [
+  {
+    img: "../backgrounds/WallpaperDog-20534536 1.png",
+    description: "We provide high quality products just for you",
+  },
+  {
+    img: "../backgrounds/WallpaperDog-20534715 1.png",
+    description: "We provide high quality products just for you",
+  },
+  {
+    img: "../backgrounds/WallpaperDog-20397673 1.png",
+    description: "We provide high quality products just for you",
+  },
+];
